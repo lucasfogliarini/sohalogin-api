d58acdb63456c0b4e5eb907b25d34651d60b653b
Lucas Fogliarini <lucasfogliarini@gmail.com>
2022-10-30 14:00:41 -0300

    CA5 - ValidatePassword

--- SohaLogin.Tests/AccountServiceTests.cs
+++ SohaLogin.Tests/AccountServiceTests.cs
@@ -1,6 +1,7 @@
 using NSubstitute;
 using SohaLogin.Accounts;
 using SohaLogin.Database;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace SohaLogin.Tests
@@ -14,29 +15,49 @@ namespace SohaLogin.Tests
         [InlineData("", "password1")]
         public void Login_ShouldThrowsException_WhenEmailAndPasswordIsEmpty(string email, string password)
         {
+            var expectedMessage = "E-mail e senha são obrigatórios para realizar o login.";
             var sohaLoginDatabase = Substitute.For<ISohaLoginDatabase>();
             var accountService = new AccountService(sohaLoginDatabase);
 
-            Assert.Throws<ValidationException>(() =>
+            var exception = Assert.Throws<ValidationException>(() =>
             {
                 accountService.Login(email, password);
             });
+            Assert.Equal(expectedMessage, exception.Message);
         }
 
         //CA04 - O usuário deverá ser um e-mail e deverá haver uma validação para caso não seja um e-mail válido
         [Theory(DisplayName = "E-mail deve ter um formato válido.")]
-        [InlineData(null)]
         [InlineData("email1")]
         [InlineData("email1@")]
         public void Login_ShouldThrowsException_WhenEmailIsInvalid(string email)
         {
+            var expectedMessage = "E-mail deve ter um formato válido. ex.: 'username@domain.com'";
             var sohaLoginDatabase = Substitute.For<ISohaLoginDatabase>();
             var accountService = new AccountService(sohaLoginDatabase);
 
-            Assert.Throws<ValidationException>(() =>
+            var exception = Assert.Throws<ValidationException>(() =>
             {
                 accountService.Login(email, "anypassword");
             });
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        //CA05 - A senha deverá ter no mínimo 4 caracteres e no máximo 15
+        [Theory(DisplayName = "A senha deverá ter no mínimo 4 caracteres e no máximo 15")]
+        [InlineData("pas")]
+        [InlineData("ThisPasswordHasMoreThan15chars")]
+        public void Login_ShouldThrowsException_WhenPasswordIsInvalid(string password)
+        {
+            var expectedMessage = "A senha deverá ter no mínimo 4 caracteres e no máximo 15";
+            var sohaLoginDatabase = Substitute.For<ISohaLoginDatabase>();
+            var accountService = new AccountService(sohaLoginDatabase);
+
+            var exception = Assert.Throws<ValidationException>(() =>
+            {
+                accountService.Login("anyemail@domain.com", password);
+            });
+            Assert.Equal(expectedMessage, exception.Message);
         }
     }
 }
--- SohaLogin/Accounts/AccountService.cs
+++ SohaLogin/Accounts/AccountService.cs
@@ -14,17 +14,27 @@ namespace SohaLogin.Accounts
 
         public void Login(string email, string password)
         {
+            ValidateLogin(email, password);
+        }
+
+        private void ValidateLogin(string email, string password)
+        {
             if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
                 throw new ValidationException("E-mail e senha são obrigatórios para realizar o login.");
 
             ValidateEmail(email);
+            ValidatePassword(password);
         }
-
         private void ValidateEmail(string email)
         {
             var mailValidator = new EmailAddressAttribute();
             if(!mailValidator.IsValid(email))
                 throw new ValidationException("E-mail deve ter um formato válido. ex.: 'username@domain.com'");
+        }
+        private void ValidatePassword(string password)
+        {
+            if (password == null || password.Length < 4 || password.Length > 15)
+                throw new ValidationException("A senha deverá ter no mínimo 4 caracteres e no máximo 15");
         }
     }
 }
